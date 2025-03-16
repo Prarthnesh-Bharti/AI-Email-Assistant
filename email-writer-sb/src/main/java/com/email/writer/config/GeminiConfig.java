05bc58fd2848d073fb897f92b37e3df27e681536
Prarthnesh Bharti <128624879+Prarthnesh-Bharti@users.noreply.github.com>
2025-03-16 17:30:49 +0530

    Update GeminiConfig.java

--- email-writer-sb/src/main/java/com/email/writer/config/GeminiConfig.java
+++ email-writer-sb/src/main/java/com/email/writer/config/GeminiConfig.java
@@ -2,27 +2,12 @@ package com.email.writer.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import lombok.Data;
 
 @Configuration
 @ConfigurationProperties(prefix = "gemini.api")
+@Data
 public class GeminiConfig {
-    
     private String url;
     private String key;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
 }
